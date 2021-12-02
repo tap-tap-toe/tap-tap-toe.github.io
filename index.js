9323531a18bb2bc22044f64fccff005f1274050f
l4t3x <pantsperopero@gmail.com>
2021-12-02 21:32:27 +0900

    the first version

---
+++ index.js
@@ -0,0 +1,15 @@
+$(window).on("load", function () {
+  function touch(self) {
+    self.off("click");
+    self.removeClass("selected");
+    loop();
+  }
+  function loop() {
+    const idx = Math.floor(Math.random() * 25);
+    const target = $($("td")[idx]).addClass("selected");
+    target.on("click", function () {
+      touch(target);
+    });
+  }
+  loop();
+});
